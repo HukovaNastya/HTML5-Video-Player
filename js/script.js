036363b8b3e3ea0cd0dd9a47e97ddf0b057a2f53
HukovaNastya <hukovanasta@gmail.com>
2025-10-05 20:03:04 +0300

    added-scrub-fn-to-the-progress

--- js/script.js
+++ js/script.js
@@ -42,6 +42,11 @@ function handleProgress(){
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
 }
+function scrub(e){
+    console.log(e);
+    const scrubTime = (e.offsetX/progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
 
 
 //Hook up the event listeners
@@ -49,7 +54,9 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range=> range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range=> range.addEventListener('mousemove', handleRangeUpdate));
+progress.addEventListener('click', scrub);
