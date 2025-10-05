8c73a175a87eb31f5cc16c6258a3f0b7b587f7aa
HukovaNastya <hukovanasta@gmail.com>
2025-10-05 19:36:32 +0300

    added_functionality_for-toggle-button

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//Get our elements
+const player = document.querySelector('.player');
+const video =  player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+console.log(player);
+
+//Build our functions
+
+function togglePlay(){
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    // console.log(method);
+    // if(video.paused){
+    //   video.play();
+    // }else {
+    //   video.paused();
+    // }
+}
+
+function updateButton(){
+    console.log(this)
+  const icon = this.paused ? '►' : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+
+//Hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
